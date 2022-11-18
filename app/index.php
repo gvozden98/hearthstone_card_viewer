fa0600fcf9a2556e730ef3d39913892a186b2bbd
gvozden98 <ognjen.gvozden98@gmail.com>
2022-11-18 19:49:33 +0100

    front page pictures changed

--- app/index.php
+++ app/index.php
@@ -22,7 +22,7 @@
         <div class="card">
           <div class="card-image">
             <figure>
-              <img src="../resoruces/4634baf2d3c266396dbee5ded8f727b395f973221c05346347973233227329b0.png" alt="Placeholder image" />
+              <img src="https://d15f34w2p8l1cc.cloudfront.net/hearthstone/3744dfa03c79d7614eb091d4bab21da82f27038e74796c709217e00f7d9fbbf8.png" alt="Placeholder image" />
             </figure>
           </div>
         </div>
@@ -30,8 +30,8 @@
       <div class="column is-one-fifth">
         <div class="card">
           <div class="card-image">
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+            <figure class="image">
+              <img src="https://d15f34w2p8l1cc.cloudfront.net/hearthstone/bc286530a2962c07e7784d4f034ef3375fce67f779ef68c98ce7551753b20ab6.png" alt="Placeholder image" />
             </figure>
           </div>
         </div>
@@ -39,8 +39,8 @@
       <div class="column is-one-fifth">
         <div class="card">
           <div class="card-image">
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+            <figure class="image">
+              <img src="https://d15f34w2p8l1cc.cloudfront.net/hearthstone/4387cee6d06e8ba368f9af46e165b52968c9dd2859a167dc1edfd94f297008a4.png" alt="Placeholder image" />
             </figure>
           </div>
         </div>
@@ -48,8 +48,8 @@
       <div class="column is-one-fifth">
         <div class="card">
           <div class="card-image">
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+            <figure class="image">
+              <img src="https://d15f34w2p8l1cc.cloudfront.net/hearthstone/fa2767b4ee1e70a61811bd1ea40d34598315a87fb01486da6e93e36c53386253.png" alt="Placeholder image" />
             </figure>
           </div>
         </div>
@@ -57,8 +57,8 @@
       <div class="column is-one-fifth">
         <div class="card">
           <div class="card-image">
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+            <figure class="image">
+              <img src="https://d15f34w2p8l1cc.cloudfront.net/hearthstone/141efe9d97f8d27d468c37334c74155eabcd3680db5fa1c4782a7c173b740fba.png" alt="Placeholder image" />
             </figure>
           </div>
         </div>
@@ -66,8 +66,8 @@
       <div class="column is-one-fifth">
         <div class="card">
           <div class="card-image">
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+            <figure class="image">
+              <img src="https://d15f34w2p8l1cc.cloudfront.net/hearthstone/d9e5664cd014df6d0a9114ce5487370b98e67f09265986feca3fb0c9348e3ebe.png" alt="Placeholder image" />
             </figure>
           </div>
         </div>
@@ -75,8 +75,8 @@
       <div class="column is-one-fifth">
         <div class="card">
           <div class="card-image">
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+            <figure class="image">
+              <img src="https://d15f34w2p8l1cc.cloudfront.net/hearthstone/8cc6f521dac86c945f12424cf0a4c4b78d73157d4797e3169c3cc8dbe2fbb9d1.png" alt="Placeholder image" />
             </figure>
           </div>
         </div>
@@ -84,8 +84,8 @@
       <div class="column is-one-fifth">
         <div class="card">
           <div class="card-image">
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+            <figure class="image">
+              <img src="https://d15f34w2p8l1cc.cloudfront.net/hearthstone/5f2a8f00741cc29c6abdd3fb0c2187687abb85d0e8db96d14b2c9cd6865b5025.png" alt="Placeholder image" />
             </figure>
           </div>
         </div>
