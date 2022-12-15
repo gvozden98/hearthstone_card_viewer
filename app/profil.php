81ebd3c84f32c512b2c20e0f95587c5318593b3f
gvozden98 <ognjen.gvozden98@gmail.com>
2022-12-15 20:52:35 +0100

    added js file to control profile page

--- app/profil.php
+++ app/profil.php
@@ -22,41 +22,47 @@ if (isset($_POST["deleteAccBtn"]) && isset($_SESSION['user_email']) && isset($_P
     }
 }
 ?>
-?>
 
 <section class="section">
     <div class="container">
-        <form method="POST" action="#" class="box">
-            <div class="field">
-                <p class="is-size-1" style="font-family: 'Irish Grover';">Profile</p>
+        <div class="columns">
+            <div class="column"></div>
+            <div class="column is-three-fifths">
+                <form method="POST" action="#" class="box">
+                    <div id="divForm">
+                        <div class="field">
+                            <p class="is-size-1" style="font-family: 'Irish Grover';">Profile</p>
+                        </div>
+                        <div class="field">
+                            <p style="font-family: 'Irish Grover';" class="is-size-3"><span class="tag is-link is-size-4">Email--></span>
+                                <?php if (isset($_SESSION['user_email'])) {
+                                    echo $email;
+                                } ?></p>
+                        </div>
+                        <div class="field is-grouped" id="btnDiv">
+                            <div class="control">
+                                <button class="button is-link is-inverted" name="btnOpenChangePass" id="btnOpenChangePass">Change password?</button>
+                            </div>
+                            <div class="control">
+                                <button class="button is-danger is-inverted" name="btnOpenDeleteAcc" id="btnOpenDeleteAcc">Delete account?</button>
+                            </div>
+                        </div>
+                        <!-- <div class="field">
+                        <label class="label" for="newPass">Password:</label>
+                        <input class="input is-danger block" type="text" placeholder="*******" name="password">
+                        <button class="button is-danger" type="submit" name="btnDeleteAcc">Delete</button>
+                    </div> -->
+                    </div>
+                </form>
             </div>
-            <div class="field">
-                <p style="font-family: 'Irish Grover';" class="is-size-3">Email --> <?php if (isset($_SESSION['user_email'])) {
-                                                                                        echo $email;
-                                                                                    } ?></p>
-            </div>
-            <div class="field is-grouped">
-                <div class="control">
-                    <button class="button is-link">Submit</button>
-                </div>
-                <div class="control">
-                    <button class="button is-link is-light">Cancel</button>
-                </div>
-                <?php
-                if (isset($_GET["error"])) {
-                    if ($_GET["error"] == "none") {
-                        echo '
-                            <div class="field">
-                                <p class="help content is-success is-large">Message sent!</p>
-                            </div>';
-                    }
-                }
-                ?>
-            </div>
-        </form>
+
+            <div class="column"></div>
+        </div>
+
     </div class="container">
 </section class="section">
 </body>
 <script src="../scripts/navbarScript.js"></script>
+<script src="../scripts/profileScript.js"></script>
 
 </html>
---
+++ scripts/profileScript.js
@@ -0,0 +1,40 @@
+const btnOpenChangePass = document.getElementById("btnOpenChangePass");
+const btnOpenDeleteAcc = document.getElementById("btnOpenDeleteAcc");
+const divForm = document.getElementById("divForm");
+
+btnOpenDeleteAcc.addEventListener("click", function () {
+  let deleteDiv = document.getElementById("deleteDiv");
+  let changeDiv = document.getElementById("changeDiv");
+  if (typeof deleteDiv != "undefined" && deleteDiv != null) {
+    deleteDiv.remove();
+  }
+  if (typeof changeDiv != "undefined" && changeDiv != null) {
+    changeDiv.remove();
+  }
+  let deleteField = document.createElement("div");
+  deleteField.classList = "field";
+  deleteField.setAttribute("id", "deleteDiv");
+  divForm.appendChild(deleteField);
+  deleteField.innerHTML = `                        <label class="label" for="password">Password:</label>
+  <input class="input is-danger block" type="password" placeholder="*******" name="password" required>
+  <button class="button is-danger" type="submit" name="btnDeleteAcc">Delete</button>`;
+});
+btnOpenChangePass.addEventListener("click", function () {
+  let deleteDiv = document.getElementById("deleteDiv");
+  let changeDiv = document.getElementById("changeDiv");
+  if (typeof deleteDiv != "undefined" && deleteDiv != null) {
+    deleteDiv.remove();
+  }
+  if (typeof changeDiv != "undefined" && changeDiv != null) {
+    changeDiv.remove();
+  }
+  let changePassField = document.createElement("div");
+  changePassField.classList = "field";
+  changePassField.setAttribute("id", "changeDiv");
+  divForm.appendChild(changePassField);
+  changePassField.innerHTML = `                        <label class="label" for="oldPassword">Old password:</label>
+    <input class="input is-link block" type="password" placeholder="*******" name="oldPassword" required>
+    <label class="label" for="newPassword">New password:</label>
+    <input class="input is-link block" type="password" placeholder="*******" name="newPassword" required>
+    <button class="button is-link" type="submit" name="btnChangePass">Change</button>`;
+});
